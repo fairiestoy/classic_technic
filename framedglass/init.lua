8594aa50dfa64050fb177fb6e30ad8970909696a
Pitriss <devnull@wjake.com>
2013-12-12 16:55:41 +0100

    Added recipes for coloured framed glass

--- framedglass/init.lua
+++ framedglass/init.lua
@@ -77,31 +77,39 @@ minetest.register_node("framedglass:steel_framed_obsidian_glass", {
 })
 
 function add_coloured_framedglass(name, desc, dye, texture)
-minetest.register_node( "framedglass:steel_framed_obsidian_glass"..name, {
-	description = "Steel-framed "..desc.." Obsidian Glass",
-	tiles = {"framedglass_steel_frame.png",texture},
-	drawtype = "glasslike_framed",
-	paramtype = "light",
-	sunlight_propagates = true,
-	is_ground_content = true,
-	use_texture_alpha = true,
-	groups = {cracky=3},
-	sounds = default.node_sound_glass_defaults(),
-}) 
+	minetest.register_node( "framedglass:steel_framed_obsidian_glass"..name, {
+		description = "Steel-framed "..desc.." Obsidian Glass",
+		tiles = {"framedglass_steel_frame.png",texture},
+		drawtype = "glasslike_framed",
+		paramtype = "light",
+		sunlight_propagates = true,
+		is_ground_content = true,
+		use_texture_alpha = true,
+		groups = {cracky=3},
+		sounds = default.node_sound_glass_defaults(),
+	})
+
+	minetest.register_craft({
+		type = "shapeless",
+		output = "framedglass:steel_framed_obsidian_glass"..name,
+		recipe = {
+			"framedglass:steel_framed_glass",
+			dye
+		}
+	})
+
 end
-
-add_coloured_framedglass ("red","Red","","framedglass_redglass.png")
-add_coloured_framedglass ("green","Green","","framedglass_greenglass.png")
-add_coloured_framedglass ("blue","Blue","","framedglass_blueglass.png")
-add_coloured_framedglass ("cyan","Cyan","","framedglass_cyanglass.png")
-add_coloured_framedglass ("darkgreen","Dark Green","","framedglass_darkgreenglass.png")
-add_coloured_framedglass ("violet","Violet","","framedglass_violetglass.png")
-add_coloured_framedglass ("pink","Pink","","framedglass_pinkglass.png")
-add_coloured_framedglass ("yellow","Yellow","","framedglass_yellowglass.png")
-add_coloured_framedglass ("orange","Orange","","framedglass_orangeglass.png")
-add_coloured_framedglass ("brown","Brown","","framedglass_brownglass.png")
-add_coloured_framedglass ("white","White","","framedglass_whiteglass.png")
-add_coloured_framedglass ("grey","Grey","","framedglass_greyglass.png")
-add_coloured_framedglass ("darkgrey","Dark Grey","","framedglass_darkgreyglass.png")
-add_coloured_framedglass ("black","Black","","framedglass_blackglass.png")
-
+add_coloured_framedglass ("red","Red","dye:red","framedglass_redglass.png")
+add_coloured_framedglass ("green","Green","dye:green","framedglass_greenglass.png")
+add_coloured_framedglass ("blue","Blue","dye:blue","framedglass_blueglass.png")
+add_coloured_framedglass ("cyan","Cyan","dye:cyan","framedglass_cyanglass.png")
+add_coloured_framedglass ("darkgreen","Dark Green","dye:dark_green","framedglass_darkgreenglass.png")
+add_coloured_framedglass ("violet","Violet","dye:violet","framedglass_violetglass.png")
+add_coloured_framedglass ("pink","Pink","dye:pink","framedglass_pinkglass.png")
+add_coloured_framedglass ("yellow","Yellow","dye:yellow","framedglass_yellowglass.png")
+add_coloured_framedglass ("orange","Orange","dye:orange","framedglass_orangeglass.png")
+add_coloured_framedglass ("brown","Brown","dye:brown","framedglass_brownglass.png")
+add_coloured_framedglass ("white","White","dye:white","framedglass_whiteglass.png")
+add_coloured_framedglass ("grey","Grey","dye:grey","framedglass_greyglass.png")
+add_coloured_framedglass ("darkgrey","Dark Grey","dye:dark_grey","framedglass_darkgreyglass.png")
+add_coloured_framedglass ("black","Black","dye:black","framedglass_blackglass.png")
